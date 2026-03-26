165ddd798ce56a35b182da7e17ea723ef42edb6a
Khushboo Bhagchandani <119107041+Khushboo2525@users.noreply.github.com>
2026-03-26 17:59:52 +0530

    Day 6: Docker setup with MySQL container and entrypoint script

---
+++ entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo "Waiting for MySQL..."
+
+while ! python -c "
+import sys
+import time
+try:
+    import MySQLdb
+    MySQLdb.connect(host='db', port=3306, user='root', passwd='root2525', db='expense_tracker_db')
+    sys.exit(0)
+except Exception as e:
+    print(e)
+    sys.exit(1)
+" ; do
+    echo "MySQL not ready, retrying in 3 seconds..."
+    sleep 3
+done
+
+echo "MySQL is ready!"
+python manage.py migrate
+python manage.py runserver 0.0.0.0:8000

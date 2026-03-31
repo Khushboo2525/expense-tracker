26ae1bf23b25594905282f377714e6d5dcaf8e45
Khushboo Bhagchandani <119107041+Khushboo2525@users.noreply.github.com>
2026-03-31 22:05:51 +0530

    Fix entrypoint to use env variables for DB connection

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,24 +2,11 @@
 
 echo "Waiting for MySQL..."
 
-DB_HOST="${DB_HOST:-localhost}"
-DB_PORT="${DB_PORT:-3306}"
-DB_USER="${DB_USER:-root}"
-DB_PASSWORD="${DB_PASSWORD:-}"
-DB_NAME="${DB_NAME:-expense_tracker_db}"
-
 while ! python -c "
 import sys
-import os
 try:
     import MySQLdb
-    MySQLdb.connect(
-        host=os.environ.get('DB_HOST', 'localhost'),
-        port=int(os.environ.get('DB_PORT', 3306)),
-        user=os.environ.get('DB_USER', 'root'),
-        passwd=os.environ.get('DB_PASSWORD', ''),
-        db=os.environ.get('DB_NAME', 'expense_tracker_db'),
-    )
+    MySQLdb.connect(host='$DB_HOST', port=int('$DB_PORT'), user='$DB_USER', passwd='$DB_PASSWORD', db='$DB_NAME')
     sys.exit(0)
 except Exception as e:
     print(e)
